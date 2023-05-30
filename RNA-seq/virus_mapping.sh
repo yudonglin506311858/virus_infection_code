85b055223f766f9b8d69c69af6a7bf344ad64309
yudonglin506311858 <506311858@qq.com>
2023-05-30 22:02:38 +0800

    Update virus_mapping.sh

--- RNA-seq/virus_mapping.sh
+++ RNA-seq/virus_mapping.sh
@@ -27,7 +27,7 @@ for i in {RD-MOCK-1,RD-EV68-24h-2,RD-EV71-24h-2};
 do 
 #cutadapt --pair-filter=any --minimum-length 15 --max-n 8 -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCAC -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGTAGATCTCGGTGGTCGCCGTATCATT -o ./clean/${i}_rmadp_1.fq.gz -p ./clean/${i}_rmadp_2.fq.gz ${i}_R1.fq.gz ${i}_R2.fq.gz >>filter.txt 2>&1
 #trimmomatic PE -threads 40 -phred33 ./clean/${i}_rmadp_1.fq.gz ./clean/${i}_rmadp_2.fq.gz -baseout ./clean/${i}_fliter.fq.gz  AVGQUAL:20 SLIDINGWINDOW:4:15 MINLEN:15 1>>filter.txt 2>&1;
-hisat2 --threads 35 -x /data/yudonglin/reference/virus/ev68/ev68 -1 ./clean/${i}_fliter_1P.fq.gz -2 ./clean/${i}_fliter_2P.fq.gz -S ${i}.sam;
+hisat2 --threads 35 -x /data/yudonglin/reference/virus/ev68/ev68 -1 ./clean/${i}_fliter_1P.fq.gz -2 ./clean/${i}_fliter_2P.fq.gz -S ${i}_ev68.sam;
 #samtools view -S ${i}.sam -b > ${i}.bam;
 #rm ./hisat/${i}.sam;
 #samtools sort ./hisat/${i}.bam -o ./hisat/${i}_sorted.bam;
